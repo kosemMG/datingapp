c244447e3583bbeaad2ff4e421652de957deb6d4
Moshe Gelberg <gelberg@pm.me>
2025-10-26 08:03:19 +0200

    Added client & CORS

---
+++ client/src/app/app.ts
@@ -0,0 +1,21 @@
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit {
+  private readonly http = inject(HttpClient);
+  protected readonly title = 'Dating app';
+  protected members = signal<any>([]);
+
+  public ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/members').subscribe({
+      next: response => this.members.set(response),
+      error: error => console.log(error),
+      complete: () => console.log('Successfully connected to the server')
+    });
+  }
+}
