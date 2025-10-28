1bff4f1516c3b4b91963c034d4f3bcd72646cf4b
Moshe Gelberg <gelberg@pm.me>
2025-10-28 18:43:12 +0200

    Added AccountController

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTO;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController(AppDbContext context) : BaseApiController
+{
+    [HttpPost("register")] // api/account/register
+    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+    {
+        if (await EmailExists(registerDto.Email)) return BadRequest("Email already exists");
+        
+        using var hmac = new HMACSHA512();
+
+        var user = new AppUser
+        {
+            Email = registerDto.Email,
+            DisplayName = registerDto.DisplayName,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+            PasswordSalt = hmac.Key
+        };
+
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+        
+        return user;
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+    {
+        var user = await context.Users.SingleOrDefaultAsync(user => user.Email == loginDto.Email);
+        if (user == null) return Unauthorized("Invalid email");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+        
+        if (!computedHash.SequenceEqual(user.PasswordHash))
+        {
+            return Unauthorized("Invalid password");
+        }
+        
+        return user;
+    }
+
+    private async Task<bool> EmailExists(string email)
+    {
+        return await context.Users.AnyAsync(user => user.Email.ToLower() == email.ToLower());
+    }
+}
