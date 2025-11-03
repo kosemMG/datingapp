2047deae4891685ee44193dfade815a82e469b35
Moshe Gelberg <gelberg@pm.me>
2025-11-03 18:05:31 +0200

    Added Members & Photos DbSets, Seeding, and MemberRepository

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,26 +1,31 @@
-using API.Data;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class MembersController(AppDbContext context) : BaseApiController
+[Authorize]
+public class MembersController(IMemberRepository repository) : BaseApiController
 {
     [HttpGet]
-    public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers() // localhost:5001/api/members
+    public async Task<ActionResult<IReadOnlyList<Member>>> GetMembers() // localhost:5001/api/members
     {
-        return await context.Users.ToListAsync();
+        return Ok(await repository.GetMembersAsync());
     }
 
-    [Authorize]
     [HttpGet("{id}")]
-    public async Task<ActionResult<AppUser>> GetMember(string id)
+    public async Task<ActionResult<Member>> GetMember(string id)
     {
-        var member = await context.Users.FindAsync(id);
+        var member = await repository.GetMemberByIdAsync(id);
         
         if (member == null) return NotFound();
         return member;
+    }
+
+    [HttpGet("{memberId}/photos")]
+    public async Task<ActionResult<IReadOnlyList<Photo>>> GetPhotosForMember(string memberId)
+    {
+        return Ok(await repository.GetPhotosForMemberAsync(memberId));
     }
 }
